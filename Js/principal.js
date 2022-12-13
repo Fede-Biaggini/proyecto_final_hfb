03173935657803a14523aa7da399252b2a775b20
Fede Biaggini <fede_biaggini@hotmail.com>
2022-12-13 14:57:00 -0300

    modificaciones_varias

---
+++ Js/principal.js
@@ -0,0 +1,3 @@
+let whatsapp = document.getElementById("whatsapp");
+whatsapp.addEventListener("click", ()=> {
+    alert("Vas a salir del sitio");});
--- Js/timbo.js
+++ Js/timbo.js
@@ -1,10 +1,11 @@
 
 //Hola, estoy comenzando mi proyecto final.
 //COMO QUEREMOS QUE SE VEA:
-//1_PORTADA
-    //BOTON DE INGRESO + INFO BASICA
+//1_PORTADA timbo.html
+    //BOTON DE INGRESO a principal.html
 //2_NOMBRE/TITULO
-    //_BREVE DESCRIPCIÓN
-    //_BOTONES DE REDIRECCION A OTRAS PAG/SECCIONES
+    //BREVE DESCRIPCIÓN
+    //BOTONES DE REDIRECCION A OTRAS PAG/otra_seccion.html
+    //Contacto.html
 
 alert("Bienvenidos a nuestra Web Oficial!");
